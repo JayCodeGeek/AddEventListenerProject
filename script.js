fb1ab8cd921114d085d485d98f4445a16842f9cd
Jayesh Bandi <154779425+JayCodeGeek@users.noreply.github.com>
2024-07-29 08:07:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementsByTagName("button")[0].addEventListener("click",function()
+{
+    
+document.querySelector(".carride").classList.add("carLeft");
+
+});
+document.getElementsByTagName("button")[1].addEventListener("click",function()
+{
+    
+document.querySelector(".carride").classList.remove("carLeft");
+
+});
+
+
+
+
